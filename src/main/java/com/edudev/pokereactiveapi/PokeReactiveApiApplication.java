34b6cf5810fc8a86e8916cdd791e4ad4fffd7cb3
Duduxs <duduxss3@gmail.com>
2020-08-18 13:18:34 -0300

    Fist commit

---
+++ src/main/java/com/edudev/pokereactiveapi/PokeReactiveApiApplication.java
@@ -0,0 +1,13 @@
+package com.edudev.pokereactiveapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeReactiveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeReactiveApiApplication.class, args);
+	}
+}
+
---
+++ src/test/java/com/edudev/pokereactiveapi/PokeReactiveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edudev.pokereactiveapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeReactiveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
